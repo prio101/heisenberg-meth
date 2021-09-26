f108692c073d734f13bab6f6af7f82064b7a20d3
Mahabub Islam Prio <miprio101@gmail.com>
2021-09-27 03:55:13 +0600

    appending result

--- task-5.py
+++ task-5.py
@@ -1,12 +1,29 @@
 import os
+import math
 
+
+def get_so_result(co_ordinates_sulfer, co_ordinates_oxygen):
+  length = len(co_ordinates_oxygen)
+  co_ordinates_sulfer_x = co_ordinates_sulfer[0]
+  co_ordinates_sulfer_y = co_ordinates_sulfer[1]
+  co_ordinates_sulfer_z = co_ordinates_sulfer[2]
+  result_set = []
+  for elem in co_ordinates_oxygen:
+    co_ordinates_oxygen_x = elem[0]
+    co_ordinates_oxygen_y = elem[1]
+    co_ordinates_oxygen_z = elem[2]  
+    result = math.sqrt(pow( (co_ordinates_sulfer_x - co_ordinates_oxygen_x ), 2) + pow( (co_ordinates_sulfer_y - co_ordinates_oxygen_y ),2) + pow((co_ordinates_sulfer_z - co_ordinates_oxygen_z ),2) )
+    result_set.append(result)
+
+    
 def find_s_o_bonds(file, line_number):
   sulfer_atomic_number = 16
   oxygen_atomic_number = 8
   current_line = line_number
-  
-
+  co_ordinates_sulfer = []
+  co_ordinates_oxygen = []
   dump_data_arr = []
+  so_bonds_results_arr = []
 
   for current_line in file:
     second_col = file[current_line].split(' ')[1]
@@ -16,7 +33,8 @@ def find_s_o_bonds(file, line_number):
     elif(oxygen_atomic_number in second_col):
       co_ordinates_oxygen = file[current_line].split(' ')[-3]  
       dump_data_arr.append(file[current_line])
-    
+  
+  get_so_result(co_ordinates_sulfer, co_ordinates_oxygen)
 
 def get_the_immidiate_lines_blocks(file, line_number):
   line_number_for_the_separator_count = 0
@@ -57,11 +75,6 @@ def main():
 
   files_list = os.listdir(path_name+folder_name)
   res = read_the_messy_files(files_list)
-
-
-
-
-
 
 if __name__ == "__main__":
   main()
