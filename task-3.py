9d90902c2beb9f9619c3cc58a25911fe2dd07cd7
Mahabub Islam Prio <miprio101@gmail.com>
2021-09-26 23:57:32 +0600

    Current Y for each file

--- task-3.py
+++ task-3.py
@@ -9,10 +9,14 @@ def name_bringer(alphanumeric_word):
   # getting the last elem [36] or [78]
   return alphanumeric_word.split('data')[-1] 
 
-def get_the_value_y_for_single_file_and_row_and_store(file_name, current_row):
+def get_the_value_y_for_single_file_and_row_and_store(file_name, current_line):
   print(file_name)
   file = open('./a1q3/'+ file_name).readlines()
-  print(file)
+  line_numbers = len(file)
+  print(line_numbers)
+  print(current_line)
+ 
+  print(file[current_line].split(' ')[0])
 
 def main():
   # file = open("./a1q3/data36.dat").readlines()
@@ -34,16 +38,21 @@ def main():
   result_file = result_file_name(start_file_name, last_file_name)
   print(result_file_name(start_file_name, last_file_name))
 
-  big_chunky_y_holder = []
-  current_row = 0
+  big_chunky_y_holder = []  
+  
+  # will change
+  max_lines_per_file = 1000
 
-  for file in list_dir:
-    y_value = get_the_value_y_for_single_file_and_row_and_store(file, current_row)
+  for current_line in range(1000):
+    for file in list_dir:
+      y_value = get_the_value_y_for_single_file_and_row_and_store(file, current_line)
     
-    # putting the single 
-    # y value into big_chunky_y_array
-    big_chunky_y_holder.append(y_value)
-    print(big_chunky_y_holder)
+      # putting the single 
+      # y value into big_chunky_y_array
+      big_chunky_y_holder.append(y_value)
+      # print(big_chunky_y_holder)
+    current_line = current_line + 1
+  
 
 
 if __name__ == "__main__":
